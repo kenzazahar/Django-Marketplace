8332a33770b451be8a50dfaf8df80da2a3718eeb
kenzazahar <kenzazahar17@gmail.com>
2024-07-16 18:32:21 +0100

    Final Project

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,6 @@ urlpatterns=[
     path('',views.index,name='index'),
     path('contact/',views.contact,name='contact'),
     path('login/',auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
+
 ]
